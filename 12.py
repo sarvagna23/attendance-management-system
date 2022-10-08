76293cfce3c72f344e7230c1ba431162f80ad83a
7ARCHER7 <103680925+7ARCHER7@users.noreply.github.com>
2022-10-09 00:06:49 +0530

    Update 12.py

--- 12.py
+++ 12.py
@@ -32,3 +32,23 @@ class AddSubjects(tk.Frame):
 			messagebox.showinfo("congratulations!", "subjects are added")
 			controller.show_frame(StartPage)
       
+
+
+
+# Python Program to find the area of triangle
+
+a = 5
+b = 6
+c = 7
+
+# Uncomment below to take inputs from the user
+# a = float(input('Enter first side: '))
+# b = float(input('Enter second side: '))
+# c = float(input('Enter third side: '))
+
+# calculate the semi-perimeter
+s = (a + b + c) / 2
+
+# calculate the area
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %area)
