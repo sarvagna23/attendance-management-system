baf8413814758b743960742c2cfdbcf3bbad841f
7ARCHER7 <103680925+7ARCHER7@users.noreply.github.com>
2022-10-09 00:03:29 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -5,3 +5,17 @@ rowname=["a","b"]
 colname=["f1","f2","f3"]
 mydataframe = pandas.DataFrame(myarray, index = rowname, columns=colname)
 print(mydataframe)
+
+
+
+
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8 
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
