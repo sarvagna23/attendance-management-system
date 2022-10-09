26397d54f8d255c1913f218a79958898d69668d3
Unanamous1230 <115394996+Unanamous1230@users.noreply.github.com>
2022-10-09 13:48:29 +0530

    Update Attendance.py

--- Attendance.py
+++ Attendance.py
@@ -7,11 +7,7 @@ class AttendanceManager(tk.Tk):
 		container.grid_rowconfigure(0,weight=1)
 		container.grid_columnconfigure(0,weight=1)
 		
-		self.frames=dict() 	
-		for F in (StartPage,NewRecord,ManageAttendance,DeleteRecord,EditRecord,AddSubjects,TodayData):
-			frame=F(container,self)
-			self.frames[F]=frame
-			frame.grid(row=0,column=0,sticky="nsew")
+		
 		
 		self.show_frame(StartPage)
 	def show_frame(self,cont):
