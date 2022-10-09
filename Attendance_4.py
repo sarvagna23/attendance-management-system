cb433174c1218cd8193ddbec0bf9d90fc530d1e4
Unanamous1230 <115394996+Unanamous1230@users.noreply.github.com>
2022-10-09 13:53:45 +0530

    Update Attendance_4.py

--- Attendance_4.py
+++ Attendance_4.py
@@ -4,7 +4,7 @@ class DeleteRecord(tk.Frame):
 		label1=tk.Label(self,text="Delete Record",font=("Times",24))
 		label2=tk.Label(self,text="This action will delete the record,continue?",font=("Times",12))
 		bt2=tk.Button(self,text="YES",bg="green",font=("Times",16),height=2,width=17,command=lambda:self.delrecord(controller))
-		bt1=tk.Button(self,text="NO",bg="yellow",font=("Times",16),height=2,width=17,command=lambda:controller.show_frame(StartPage))
+		
 		bt3=tk.Button(self,text="Back to home",bg="red",font=("Times",16),height=2,width=17,command=lambda:controller.show_frame(StartPage))
 		label1.pack()
 		label2.pack()
