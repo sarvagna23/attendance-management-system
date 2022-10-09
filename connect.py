38d9693d4e942da71794faf694e368cc14f8545a
Jaam456 <115415446+Jaam456@users.noreply.github.com>
2022-10-09 21:10:13 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -8,7 +8,3 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-
-mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
-
-#If this page is executed with no error, the table "customers" now has a primary key
